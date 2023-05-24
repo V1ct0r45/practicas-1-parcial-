d421398c5b773c62b1b65180f159f5c9605f945b
V1ct0r45 <125516286+V1ct0r45@users.noreply.github.com>
2023-05-24 21:39:50 +0000

    actu

---
+++ practica 7/prog6.c
@@ -0,0 +1,54 @@
+/**
+ * @file prog6.c
+ * @author Ramirez Aguilar Victor Saul (victors.ramireza@gmail.com)
+ * @boleta 2023640349
+ * @brief 
+ * @version 0.1
+ * @date 2023-05-22
+ * 
+ * @copyright GPL v3
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    int n;
+    printf("Ingrese el tamaño del arreglo: ");
+    scanf("%d", &n);
+    int *arr = (int*)malloc(n * sizeof(int));
+    srand(time(NULL));
+
+    for (int i = 0; i < n; i++){
+        *(arr + i) = (rand() % 2001)-1000;
+        
+    }
+
+    for (int i = 0; i < n-1; i++)
+    {
+        for (int j = 0; j < n-i-1; j++)
+        {
+            if (*(arr + j) > *(arr + j + 1))
+            {
+                int aux = *(arr + j);
+                *(arr + j) = *(arr + j + 1);
+                *(arr + j + 1) = aux;
+            }
+            
+        }
+        
+    }
+
+    printf("Los valores del arreglo ordenado son:\n ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", *(arr + i));
+    }
+    
+    
+    
+    return 0;
+}
